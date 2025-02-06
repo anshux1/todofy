246eea21150c24b23ff5ba6ab9b11a24e0b7ede8
Anshu Kumar singh <anshukrsingh11@gmail.com>
2025-02-06 16:31:16 +0530

    feat: add husky pre-commit hook for linting and building

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,34 +1,34 @@
-import prisma from '../src/db';
+import prisma from "../src/db"
 
 async function seedUsers() {
   try {
     await prisma.user.upsert({
       where: { email: "example@gmail.com" },
-      update: { email: "example@gmail.com", },
+      update: { email: "example@gmail.com" },
       create: {
         email: "example@gmail.com",
         password: "1234",
-        name: "example"
+        name: "example",
       },
-    });
+    })
   } catch (error) {
-    console.error('Error seeding users: ', error);
-    throw error;
+    console.error("Error seeding users: ", error)
+    throw error
   }
 }
 
 async function seedDatabase() {
   try {
-    await seedUsers();
+    await seedUsers()
   } catch (error) {
-    console.error('Error seeding database:', error);
-    throw error;
+    console.error("Error seeding database:", error)
+    throw error
   } finally {
-    prisma.$disconnect();
+    prisma.$disconnect()
   }
 }
 
 seedDatabase().catch((error) => {
-  console.error('An unexpected error occurred during seeding:', error);
-  process.exit(1);
-});
+  console.error("An unexpected error occurred during seeding:", error)
+  process.exit(1)
+})
